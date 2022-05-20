fd80b2c706a3afd1902749de60f00024d14862f6
Luan Braz <luan_silvacpv@hotmail.com>
2022-05-19 21:38:37 -0300

    estudo sobre classes

--- curso-java-udemy/src/classe/Produto.java
+++ curso-java-udemy/src/classe/Produto.java
@@ -4,9 +4,31 @@ public class Produto {
 	
 	String nome;
 	double preco;
-	double desconto;
+	static double desconto = 0.25;
 	
+	Produto() {
+		
+	}
+	
+//	Produto(String nomeInicial) {
+//		nome = nomeInicial;
+//	}
+//	
+//	Produto(String nomeInicial, double precoInicial) {
+//		nome = nomeInicial;
+//		preco = precoInicial;
+//	}
+	
+	Produto(String nomeInicial, double precoInicial) {
+		nome = nomeInicial;
+		preco = precoInicial;
+	}
+		
 	double precoComDesconto() {
 		return preco * (1 - desconto);
+	}
+	
+	double precoComDesconto(double descontoDoGerente) {
+		return preco * (1-desconto + descontoDoGerente);
 	}
 }
